60c75b39baa79afdfc9e8fe1d1c7b734cda11bd4
Luisa Gaxiola <luisa.gaxiola@uadeo.mx>
2024-02-08 14:06:21 -0700

    Cambios

--- PFundamentos/Program.cs
+++ PFundamentos/Program.cs
@@ -9,7 +9,7 @@ namespace PFundamentos
     {
         static void Main(string[] args)
         {
-            SustitucionNombres();
+            Arreglo2();
             void Ejemplo1()
             {
                 int n1 = 5;
@@ -60,13 +60,114 @@ namespace PFundamentos
             }
             void SustitucionNombres()
             {
-                string Nombre = "Carlos Ojeda";
+                string Nombre = "Carlos Peña";
                 string nsust()
                 {
                     return Nombre.Replace("Carlos", "Carla");
                 }
                 Console.WriteLine(nsust());
             }
+            void CondicionalMultiple()
+            {
+                int mes;
+                Console.WriteLine("Ingresa el número del mes:");
+                mes = int.Parse(Console.ReadLine());
+
+                switch(mes) 
+                {
+                    case 1:
+                        Console.WriteLine("Enero");
+                        break;
+                    case 2:
+                        Console.WriteLine("Febrero");
+                        break;
+                    case 3:
+                        Console.WriteLine("Marzo");
+                        break;
+                    case 4:
+                        Console.WriteLine("Abril");
+                        break;
+                    case 5:
+                        Console.WriteLine("Mayo");
+                        break;
+                    case 6:
+                        Console.WriteLine("Junio");
+                        break;
+                    case 7:
+                        Console.WriteLine("Julio");
+                        break;
+                    case 8:
+                        Console.WriteLine("Agosto");
+                        break;
+                    case 9:
+                        Console.WriteLine("Septiembre");
+                        break;
+                    case 10:
+                        Console.WriteLine("Octubre");
+                        break;
+                    case 11:
+                        Console.WriteLine("Noviembre");
+                        break;
+                    case 12:
+                        Console.WriteLine("Diciembre");
+                        break;
+                    default:
+                        Console.WriteLine("No existe ese mes.");
+                        break;
+                }
+
+            }
+            #region Arreglos
+            void Arreglo1()
+            {
+                double[] n1 = new double[5];
+
+                n1[0] = 5;
+                n1[1] = 4;
+                n1[2] = 3;
+                n1[3] = 2;
+                n1[4] = 1;
+
+                for (int i = 0; i < n1.Length; i++)
+                {
+                    Console.WriteLine(n1[i]);
+                }
+
+                //string[] s1 = { "uno", "dos", "tres" };
+
+            }
+            void Arreglo2()
+            {
+                int n = 0;
+                string[] ListaMandado = new string[5];
+                double[] PrecioArticulos = new double[5];
+
+                Console.WriteLine("-- INGRESE LOS ARTICULOS AL CARRITO DE COMPRAS --");
+                for (int i = 0; i < ListaMandado.Length; i++)
+                {
+                    n++;
+                    Console.WriteLine($"{n}.");
+                    string res = Console.ReadLine();
+                    ListaMandado[i] = res;
+                    Console.WriteLine("Precio: ");
+                    double precio = double.Parse(Console.ReadLine());
+                    PrecioArticulos[i] = precio;
+                }
+                Console.Clear();
+                double Subtotal = 0, Total = 0;
+                for(int x = 0; x < 5; x++)
+                {
+                    Subtotal = PrecioArticulos[x];
+                    Total = Total + Subtotal;
+                }
+                Console.WriteLine(" -- LISTA DE MANDADO --");
+                for (int y = 0; y < 5; y++)
+                {
+                    Console.WriteLine($"{ListaMandado[y]}  -  ${PrecioArticulos[y]}");
+                }
+                Console.WriteLine($"Total: ${Total}");
+            }
+            #endregion
             Console.ReadLine();
         }
     }
